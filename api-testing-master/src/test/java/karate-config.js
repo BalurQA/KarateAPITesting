640185578fe732ab0cf0f1e4224874f879d1bcb6
Sangameshwar Balur <sangameshwar.balur@thermofisher.com>
2018-09-18 15:47:47 -0400

    updated

--- api-testing-master/src/test/java/karate-config.js
+++ api-testing-master/src/test/java/karate-config.js
@@ -8,11 +8,11 @@ function() {
     env: env,
   }
   if (env == 'qa2') {
-    baseURL: 'https://qa2-api.fishersci.com/fishersci-cart'
+    baseURL: 'qa2'
   } else if (env == 'qa1') {
-    baseURL: 'https://qa1-api.fishersci.com/fishersci-cart'
+    baseURL: 'qa1'
   } else if(env == 'dev4') {
-          baseURL: 'https://dev4-api.fishersci.com/fishersci-cart'
+          baseURL: 'dev4'
     }
 
   karate.configure('connectTimeout', 5000);
